acf8098bf7ac6e998ea36aebb10222a312b8eedb
ChandruV2003 <chandruvasu@icloud.com>
2025-06-05 00:23:09 -0400

    Additionals

---
+++ BaselineSMA/backtests/2025-06-05_00-03-58/code/main.py
@@ -0,0 +1,18 @@
+from AlgorithmImports import *
+
+class BaselineSMA(QCAlgorithm):
+    def Initialize(self):
+        self.SetStartDate(2012, 1, 1)
+        self.SetCash(10_000)
+        self.spy = self.AddEquity("SPY", Resolution.Daily).Symbol
+        self.fast = self.SMA(self.spy, 50, Resolution.Daily)
+        self.slow = self.SMA(self.spy, 200, Resolution.Daily)
+        self.SetWarmUp(200)
+
+    def OnData(self, data):
+        if self.IsWarmingUp:
+            return
+        if self.fast.Current.Value > self.slow.Current.Value and not self.Portfolio.Invested:
+            self.SetHoldings(self.spy, 1)
+        elif self.fast.Current.Value < self.slow.Current.Value and self.Portfolio.Invested:
+            self.Liquidate(self.spy)
